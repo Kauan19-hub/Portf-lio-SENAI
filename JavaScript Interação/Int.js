009258a5af134a74f5e5a5396bfceacd1d24b8e2
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2025-09-03 07:45:36 -0300

    Delete JavaScript Interação directory

--- JavaScript Interação/Int.js
+++
@@ -1,9 +0,0 @@
-//Solicita número inteiro
-let numero = parseInt(prompt("Digite um número inteiro: "))
-
-if (numero % 2 === 0) {
-   alert(`${numero} é par! `)
-
-} else {
-   alert(`${numero} é ímpar! `)
-}
