31d06e8ea174e18cc5520ca79ce8b26149aa2998
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2025-09-09 23:46:01 -0300

    :construction:

--- JavaScript Geral/Scripts.js
+++ JavaScript Geral/Scripts.js
@@ -12,159 +12,7 @@ function enter(input, checkFunction) {
 
         }
     })
-}
-
-/* Validador de formulário. 8 caracteres, 1 especial, maiúsculas e minúsculas. 
-let inputNome = window.document.querySelector("input#nome");
-
-let inputCPF = window.document.querySelector("input#cpf");
-
-let inputEmail = window.document.querySelector("input#email")
-
-let inputSenha = window.document.querySelector("input#senha")
-
-let radioMasculino = window.document.querySelector("input#masculino")
-
-let radioFeminino = window.document.querySelector("input#feminino")
-
-let radioOutro = window.document.querySelector("input#outro")
-
-let checkTecnologia = window.document.querySelector("input#tecnologia")
-
-let checkEsportes = window.document.querySelector("input#esportes")
-
-let checkMusica = window.document.querySelector("input#musica")
-
-let selectEstado = window.document.querySelector("select#estado")
-
-let buttonSubmit = window.document.getElementsByTagName("button")[0];
-
-
-
-const verificaEspacos = (nome) =>{
-    let quantEspacos = 0;
-    
-    for(let ch of nome){
-        if(ch == ' ' ){
-            quantEspacos += 1
-        }
-    }
-
-    return quantEspacos
-}
-
-//Validação cpf
-const validacaoCPF = (cpf) => {
-    let numCPF=0;
-    let operador=10
-
-    let d10=0, d11=0
-
-    if(cpf.length == 11){
-        for(let i = 0; i < 9; i++){ 
-            numCPF += operador * parseInt(cpf[i] )
-            operador -= 1
-        }
-        
-        if(numCPF < 2){
-            d10 = 0
-        }else{
-            d10 = 11 - (numCPF % 11)
-        }
-        
-        
-        numCPF = 0
-        operador = 11
-
-        for (let i = 0; i < 10; i++) {
-            numCPF += operador * parseInt(cpf[i])
-            operador -= 1
-        }
-        
-        if (numCPF < 2) {
-            d11 = 0
-        } else {
-            d11 = 11 - (numCPF % 11)
-        }
-       
-        
-       if(cpf[9] != d10 || cpf[10] != d11){
-            alert("CPF inválido!")
-            return true
-       }
-
-
-    }else{
-        alert("CPF inválido!")
-        return true
-    }
-
-    return false
-}
-
-//Valores
-let nomeCompleto, cpf, email, masculino, feminino, tecnologia, esportes, musica
-
-let erros = false
-
-buttonSubmit.addEventListener("click", ()=>{
-    let nomeCompleto = inputNome.value 
-    let cpf=inputCPF.value
-    let email = inputEmail.value
-    let senha = inputSenha.value
-
-    let masculino = radioMasculino.checked
-    let feminino = radioFeminino.checked
-    let tecnologia = checkTecnologia.checked
-    let esportes = checkEsportes.checked
-    let musica = checkMusica.checked
-    
-    let estado = selectEstado.value
-
-    let gostos = []
-    let genero
-
-    //Validação de nome
-    if(nomeCompleto[0] != " " && verificaEspacos(nomeCompleto) == 1){
-
-    }else{
-        alert("Nome inválido!")
-        erros = true
-    }
-
-    //Validação de cpf
-    erros = validacaoCPF(cpf)
-
-    //genero
-    genero = masculino ? "masculino" : feminino ? "feminino" : "outro"
-
-    //gostos
-    if(tecnologia){
-        gostos.push("tecnologia")
-    }  
-
-    if(esportes){
-        gostos.push("Esportes")
-
-    }
-
-    if(musica){
-        gostos.push("Música")
-    }
-
-    
-    //estado
-    if(estado == ""){
-        alert("Selecione um estado!")
-        erros = true
-    }
-
-    if(!erros){
-        document.body.innerHTML = `<!DOCTYPE html>
-
- SÓ EDITAR...
-*/
-
+};
 
 enter('idade', checkIdade)
 enter('numero', checkParImpar)
@@ -464,13 +312,81 @@ function animateWrite(element, text, speed = 90) {
 }
 
 function checkForms() {
-    const name = document.getElementById('name').value
-    const cpf = document.getElementById('cpf').value
-    const email = document.getElementById('email').value
-    const password = document.getElementById('password').value
-    const gender = document.getElementById('gender').value
-    const feedback = document.getElementById('r9')
+    let inputName = document.getElementById('name').value.trim()
+    let inputCPF = document.getElementById('cpf').value.trim()
+    let inputEmail = document.getElementById('email').value.trim()
+    let inputPassword = document.getElementById('password').value.trim()
+    let selectGender = document.getElementById('gender').value.trim()
+    let feedback = document.getElementById('r9')
 
+    if (inputName === "") {
+        updateResult(feedback, "Por favor, digite seu nome", "error")
+        return
+
+    }
+
+    if (inputName.length < 5) {
+        updateResult(feedback, "Seu nome precisa ter ao menos 5 caracteres", "warning")
+        return
+
+    }
+
+    const verifyCPF = c => {
+        c = c.replace(/\D/g,"");
+        return c.length == 11 &&
+            !/^(\d)\1+$/.test(c) &&
+            [9, 10].every(j => 
+                (c.slice(0, j).split("").reduce((s, n, i) => s + n * (j + 1 - i), 0) * 10 % 11 % 10) == c[j]
+
+        );
+
+    }
+        
+    if (inputCPF === "") {
+        updateResult(feedback, "Erro. Digite seu CPF", "error")
+        return
+
+    }
+
+    if (!verifyCPF(inputCPF)) {
+        updateResult(feedback, "CPF inválido", "error")
+        return
+
+    }
+
+    const emaill = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    if (inputEmail === "") {
+        updateResult(feedback, "Erro. Digite seu email", "error")
+        return
+
+    }
+
+    if (!emaill.test(inputEmail)) {
+        updateResult(feedback, "Email inválido", "error")
+        return
+
+    }
+
+    if (inputPassword === "") {
+        updateResult(feedback, "Inválido. Por favor, digite uma senha", "error")
+        return
+
+    }
+
+    if (inputPassword.length < 3) {
+        updateResult(feedback, "A senha precisa ter no mínimo 3 caracteres", "warning")
+        return
+
+    }
+
+    if (selectGender === "") {
+        updateResult(feedback, "Por favor, selecione seu gênero", "error")
+        return
+
+    }
+
+    updateResult(feedback, "Cadastro realizado!", "success")
 
 }
 
